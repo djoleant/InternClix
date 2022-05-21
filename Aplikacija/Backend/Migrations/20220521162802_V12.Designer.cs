46de57a91aff34b4dd849f4b5c2248158dbf5142
Djordje Antic <djordje.antic@elfak.rs>
2022-05-21 18:29:33 +0200

    [Backend] Dodavanje atributa CV modelu

--- Aplikacija/Backend/Migrations/20220521162802_V12.Designer.cs
+++ Aplikacija/Backend/Migrations/20220521162802_V12.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Models;
 
@@ -11,9 +12,10 @@ using Models;
 namespace Backend.Migrations
 {
     [DbContext(typeof(InternClixDbContext))]
-    partial class InternClixDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220521162802_V12")]
+    partial class V12
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Aplikacija/Backend/Migrations/20220521162802_V12.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    public partial class V12 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "CVs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "CVs");
+        }
+    }
+}
