d2e3618e43b8d5e6e8caae1f5367fb902507f705
Matija Speletic <matijasp@elfak.rs>
2022-05-31 19:09:18 +0200

    [Frontend] Pretraga i filtriranje apliciranih studenata

--- Aplikacija/Frontend/react-app/src/components/InternshipPage/ApplicantList.js
+++ Aplikacija/Frontend/react-app/src/components/InternshipPage/ApplicantList.js
@@ -1,4 +1,4 @@
-import { Button, Divider, Grid, Typography } from "@mui/material";
+import { MenuItem, Select, FormControl, InputLabel, Button, Grid, Typography, TextField } from "@mui/material";
 import { useEffect, useState } from "react";
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
@@ -8,6 +8,9 @@ import { useNavigate } from "react-router-dom";
 import { Box } from "@mui/system";
 import MatchPercentage from "./MatchPercentage";
 import ApplicantInfo from "./ApplicantInfo";
+import Slider from "./Slider";
+import ArrowDownwardRoundedIcon from '@mui/icons-material/ArrowDownwardRounded';
+import ArrowUpwardRoundedIcon from '@mui/icons-material/ArrowUpwardRounded';
 
 
 export default function ApplicantList({ internshipId, internshipSkills }) {
@@ -51,6 +54,10 @@ export default function ApplicantList({ internshipId, internshipSkills }) {
         // }
     ]);
 
+    const [minMatch, setMinMatch] = useState(0);
+    const [sortDirection, setSortDirection] = useState("none");
+    const [searchVal, setSearchVal] = useState("");
+
     const getApplicants = async () => {
         const response = await fetch("http://localhost:7240/Internship/GetAppliedStudents/" + internshipId, {
             credentials: "include"
@@ -82,32 +89,70 @@ export default function ApplicantList({ internshipId, internshipSkills }) {
 
     return (
         <Grid container sx={{ width: 1 }}>
+            <Grid item container xs={12}>
+                <Grid item md={3} xs={6}>
+                    <Typography>Min. match percentage:</Typography>
+                </Grid>
+                <Grid item md={3} xs={6}>
+                    <Slider defaultValue={0} valueLabelDisplay="on" onChange={(e, value) => { setMinMatch(value) }} />
+                </Grid>
+                <Grid item md={4} xs={8}>
+                    <TextField
+                        variant="standard"
+                        label="Search"
+                        sx={{ mt: -3, ml: 4, pr: 4 }}
+                        onChange={(e) => { setSearchVal(e.target.value) }}
+                        fullWidth
+                    />
+                </Grid>
+                <Grid item md={2} xs={4}>
+                    <Button
+                        endIcon={
+                            sortDirection == "desc" ?
+                                <ArrowDownwardRoundedIcon /> :
+                                sortDirection == "asc" ?
+                                    <ArrowUpwardRoundedIcon /> : <></>
+                        }
+                        onClick={() => {
+                            if (sortDirection == "asc") setSortDirection("desc");
+                            else if (sortDirection == "desc") setSortDirection("none");
+                            else setSortDirection("asc");
+                        }}
+                    >
+                        Sort
+                    </Button>
+                </Grid>
+
+            </Grid>
 
             <Grid item container xs={12}>
 
                 {
-                    applicants.map((applicant, index) => (
-                        <Accordion variant="outlined" sx={{ width: 1 }} key={index}>
-                            <AccordionSummary
-                                expandIcon={<ExpandMoreIcon />}
-                                aria-controls="panel1a-content"
-                                id="panel1a-header"
-                            >
-                                <Box sx={{ display: "flex", justifyContent: "space-between", width: 1 }}>
-                                    <Typography>
-                                        {(index + 1) + ")  " + applicant.name + " " + applicant.lastName}
-                                    </Typography>
-                                    <Box sx={{ width: "25%" }}>
-                                        <MatchPercentage value={applicant.match} />
-                                    </Box>
+                    applicants
+                        .filter((a) => a.match >= minMatch && (a.name + " " + a.lastName).toLowerCase().includes(searchVal.toLowerCase()))
+                        .sort((a1, a2) => (sortDirection == "desc") ? a2.match - a1.match : (sortDirection == "asc") ? a1.match - a2.match : 0)
+                        .map((applicant, index) => (
+                            <Accordion variant="outlined" sx={{ width: 1 }} key={index}>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="panel1a-content"
+                                    id="panel1a-header"
+                                >
+                                    <Box sx={{ display: "flex", justifyContent: "space-between", width: 1 }}>
+                                        <Typography>
+                                            {applicant.name + " " + applicant.lastName}
+                                        </Typography>
+                                        <Box sx={{ width: "25%" }}>
+                                            <MatchPercentage value={applicant.match} />
+                                        </Box>
 
-                                </Box>
-                            </AccordionSummary>
-                            <AccordionDetails>
-                                <ApplicantInfo internshipSkills={internshipSkills} applicant={applicant} />
-                            </AccordionDetails>
-                        </Accordion >
-                    ))
+                                    </Box>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    <ApplicantInfo internshipSkills={internshipSkills} applicant={applicant} />
+                                </AccordionDetails>
+                            </Accordion >
+                        ))
                 }
 
             </Grid>
