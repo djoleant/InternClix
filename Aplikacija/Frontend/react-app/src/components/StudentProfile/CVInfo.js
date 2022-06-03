7041249c43ac935b03f8048b6aeb5b954aebc13a
Matija Speletic <matijasp@elfak.rs>
2022-06-03 16:32:08 +0200

    [Frontend] Izmenjen prikaz CV kod StudentProfile

--- Aplikacija/Frontend/react-app/src/components/StudentProfile/CVInfo.js
+++ Aplikacija/Frontend/react-app/src/components/StudentProfile/CVInfo.js
@@ -1,11 +1,16 @@
-import { Button, Divider, Grid, Typography } from "@mui/material";
+import { Button, Divider, Grid, Typography, Box } from "@mui/material";
 import { useState, useEffect } from "react";
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { useNavigate } from "react-router-dom";
-
+import SkillChips from "../InternshipPage/SkillChips";
+import AddressIcon from '@mui/icons-material/HomeRounded';
+import PhoneIcon from '@mui/icons-material/PhoneRounded';
+import LanguageIcon from '@mui/icons-material/LanguageRounded';
+import SkillIcon from '@mui/icons-material/MiscellaneousServicesRounded';
+import TitleIcon from '@mui/icons-material/TitleRounded';
 
 
 export default function CVInfo({ cvInfo }) {
@@ -66,20 +71,40 @@ export default function CVInfo({ cvInfo }) {
                         <Typography>Basic info</Typography>
                     </AccordionSummary>
                     <AccordionDetails>
-                        <Typography align="left" ml={4} >{"City: " + info.city}</Typography>
-                        <Typography align="left" ml={4} >{"Address: " + info.address}</Typography>
-                        <Typography align="left" ml={4} >{"Phone: " + info.phone}</Typography>
-                        <Typography align="left" ml={4} >{"Title: " + info.title}</Typography>
-                        <Typography align="left" ml={4} >Skills:
-                            {
-                                info.skills.map((s, index) => (<span key={index}>{" " + s.label + ", "}</span>))
-                            }
+                        <Typography align="left" ml={4} sx={{ fontWeight: "bold", display: "flex", alignItems: "center", gap: 1 }}>
+                            <AddressIcon />
+                            {"Address:"}
                         </Typography>
-                        <Typography align="left" ml={4} >Languages:
-                            {
-                                info.languages.map((s, index) => (<span key={index}>{" " + s.title + ", "}</span>))
-                            }
+                        <Typography align="left" ml={7} >{info.address + ", " + info.city}</Typography>
+                        <Typography align="left" ml={4} mt={2} sx={{ fontWeight: "bold", display: "flex", alignItems: "center", gap: 1 }}>
+                            <PhoneIcon />
+                            {"Phone:"}
                         </Typography>
+                        <Typography align="left" ml={7} >{info.phone}</Typography>
+                        <Typography align="left" ml={4} mt={2} sx={{ fontWeight: "bold", display: "flex", alignItems: "center", gap: 1 }}>
+                            <TitleIcon />
+                            {"CV Title:"}
+                        </Typography>
+                        <Typography align="left" ml={7} >{info.title}</Typography>
+                        <Typography align="left" ml={4} mt={2} sx={{ fontWeight: "bold", display: "flex", alignItems: "center", gap: 1 }}>
+                            <LanguageIcon />
+                            {"Languages:"}
+                        </Typography>
+                        {
+                            info.languages.map((lang, index) => (
+                                <Typography key={index} align="left" ml={7} >
+                                    {lang.title + " - " + lang.description}
+                                </Typography>
+                            ))
+                        }
+                        <Typography align="left" ml={4} mt={2} sx={{ fontWeight: "bold", display: "flex", alignItems: "center", gap: 1 }}>
+                            <SkillIcon />
+                            {"Skills:"}
+                        </Typography>
+                        <Box sx={{ ml: 7, mt: 1 }}>
+                            <SkillChips skills={info.skills.map(s => ({ name: s.label }))} variant="outlined" />
+
+                        </Box>
                     </AccordionDetails>
                 </Accordion >
                 <Accordion variant="outlined" sx={{ width: 1 }} >
