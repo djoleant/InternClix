e070fc0c1473aef9c305a26c49107c2f7fd62cc4
Djordje Antic <djordje.antic@elfak.rs>
2022-06-25 02:01:04 +0200

    [Frontend] Internships page i component nadogradnja

--- Aplikacija/Frontend/react-app/src/Internships.js
+++ Aplikacija/Frontend/react-app/src/Internships.js
@@ -10,50 +10,46 @@ import CardActions from "@mui/material/CardActions";
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import TextField from '@mui/material/TextField';
 import ComboBox from './components/ComboBox';
+import { useNavigate } from "react-router-dom";
+import InternshipCard from './components/InternshipCard';
+import background from "./resources/stojny.jpg";
+
 
 export default function Internships(props) {
 
+    const [internshipData, setInternshipData] = useState(null);
+
+    const [backgroundColor, setColor] = useState("blue");
+
+
     const getInternships = async () => {
-        const response = await fetch("http://localhost:7240/Internship/GetInternship", {
-            credentials: "include"
-        });
+        const response = await fetch(
+            "http://localhost:7240/Internship/GetInternships",
+            {
+                credentials: "include",
+            }
+        );
         if (response.ok) {
             const fetchData = await response.json();
             console.log(fetchData);
-            setInternship(fetchData.internship);
+            setInternshipData(fetchData.internships);
         }
+    };
 
-    }
-
-    const [internshipData, setInternship] = useState({
-        title: "",
-        description: "",
-        duration: "",
-        compensation: "",
-        skills: [
-            { id: "", name: [] }
-        ],
-        categories: [
-            { id: "", name: [] }
-        ],
-
-    });
+    const [search, setSearch] = useState("");
 
     useEffect(() => {
         getInternships();
+        console.log("a");
     }, []);
 
-    const card = {
-        name: "Data Science Intern",
-        description:
-            "We are looking for ambitious data science interns. Expand your knowledge and get hired. Don't waste time and apply now!",
-        duration: "3 months",
-        compensation: "300 eur",
-        //skills ?
-        //categories ?
+    console.log(internshipData);
 
-    };
-    const [cards, setCards] = React.useState([card]);
+    const navigate = useNavigate();
+
+
+
+    // const [cards, setCards] = React.useState([card]);
 
 
     return (
@@ -65,19 +61,21 @@ export default function Internships(props) {
                     sx={{ p: 3, mb: 4 }}
                     variant="outlined"
                 >
-                    {/* <Box
-                        component="form"
-                        sx={{
-                            '& > :not(style)': { m: 1, width: '25ch' },
-                        }}
-                        noValidate
-                        autoComplete="off"
-                    >
-                        <TextField id="outlined-basic" label="Search" variant="outlined" />
+                    <Grid>
+                        <Grid item xs={12} style={{ padding: "10px", maxWidth: 500 }}>
+                            <TextField
+                                onChange={(event) => { setSearch(event.target.value) }}
+                                id="outlined-basic-email"
+                                label="Search"
+                                variant="outlined"
+                                fullWidth
+                            />
+                        </Grid>
 
-                    </Box> */}
+                    </Grid>
 
-                    <Grid
+
+                    {/* <Grid
                         container
                         spacing={0}
                         alignItems="center"
@@ -85,100 +83,32 @@ export default function Internships(props) {
                     >
 
                         <Grid item xs={3}>
-                        <ComboBox />
+                            <ComboBox />
                         </Grid>
 
-                    </Grid>
+                    </Grid> */}
 
 
-                    {/* <Box alignItems="center"
-                        justifyContent="center">
-                        <ComboBox />
-                    </Box> */}
+                    {internshipData != null && (
+                        <Grid container spacing={1}
+                            alignItems="center"
+                            justifyContent="center">
 
-                    <Box sx={{ mb: 3 }} variant="outlined">
-                        <Divider sx={{ mt: 5, mb: 3 }} > Available internships </Divider>
-                        <Typography component="h1" align="center" sx={{ m: 2, color: "#bbbbbb" }}>
-                            {internshipData.about}
-                        </Typography>
-                    </Box>
+                            {internshipData.internships
+                                .filter(c => c.title.toLowerCase().includes(search.toLowerCase()))
+                                .map((cards, index) => {
+                                    const { title, description, duration, compensation, skills, categories } = cards;
+                                    console.log(cards);
+                                    return (<>
+                                        <div> </div>
+                                        <InternshipCard index={index} title={title} description={description} duration={duration} compensation={compensation} skills={skills} categories={categories} />
+                                    </>
 
+                                    );
+                                })}
+                        </Grid>
+                    )}
 
-                    <Grid container spacing={1}
-                        alignItems="center"
-                        justifyContent="center">
-                        {cards.map((cards, index) => {
-                            const { image, name, description, duration, compensation } = cards;
-                            return (
-                                <Grid item>
-                                    <Card key={index}>
-                                        {/* <CardHeader
-                                            avatar={
-                                                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                                                    ES
-                                                </Avatar>
-                                            }
-                                            
-                                            title="Empcode solutions"
-                                        //subheader="moze i ne mora da bude ovo"
-                                        /> */}
-                                        <CardMedia image={image} />
-                                        <CardContent >
-
-                                            <Typography
-                                                className={"MuiTypography--heading"}
-                                                variant={"h6"}
-                                                gutterBottom
-                                            >
-                                                {name}
-                                            </Typography>
-                                            <Typography display="block"
-                                                className={"MuiTypography--subheading"}
-                                                variant={"caption"}
-
-                                            >
-                                                {description}
-                                            </Typography>
-                                            <Typography display="block"
-                                                className={"MuiTypography--subheading"}
-                                                variant={"body2"}
-                                            >
-                                                {duration}
-                                            </Typography>
-                                            <Typography
-                                                className={"MuiTypography--subheading"}
-                                                variant={"substring2:h6"}
-                                            >
-                                                {compensation}
-                                            </Typography>
-
-                                        </CardContent>
-                                        <Grid container style={{ marginTop: 3, display: "flex", flexDirection: "row", justifyContent: "center" }} spacing={3} sx={{ mb: 2 }}>
-                                            <Button style={{ marginLeft: 7, marginTop: 7 }} variant="contained" >
-                                                React
-                                            </Button>
-                                            <Button style={{ marginLeft: 7, marginTop: 7 }} variant="contained" >
-                                                .NET
-                                            </Button>
-                                            <Button style={{ marginLeft: 7, marginTop: 7 }} variant="contained" >
-                                                NodeJS
-                                            </Button>
-                                        </Grid>
-                                        <Divider light />
-                                        <CardActions>
-                                            <Button size="small" color="primary">
-                                                Apply
-                                            </Button>
-                                            <Button size="small" color="primary">
-                                                Add to favorites
-                                            </Button>
-
-                                        </CardActions>
-                                    </Card>
-                                </Grid>
-                            );
-                        })}
-                    </Grid>
 
                 </Paper>
 
---
+++ Aplikacija/Frontend/react-app/src/components/InternshipCard.js
@@ -0,0 +1,54 @@
+import Container from '@mui/material/Container';
+import React, { useState } from 'react';
+import {
+    Button,
+    Typography
+} from '@mui/material';
+import { Paper, CssBaseline, Box, Divider, Grid, Chip } from '@mui/material';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import QueryBuilderIcon from '@mui/icons-material/QueryBuilder';
+import PaidIcon from '@mui/icons-material/Paid';
+
+export default function InternshipCard(props) {
+
+    return (
+        <Container component="main"  >
+            <CssBaseline />
+            <React.Fragment>
+                <Paper
+                    sx={{ p: 2, mb: 2, ml: 2 }}
+                    variant="outlined"
+                >
+
+                    <Grid container style={{ marginTop: 3, marginLeft: 3, display: "flex", flexDirection: "column", alignItems: "flex-start" }} spacing={3} sx={{ mb: 4 }}>
+                        <Typography align="center" sx={{ m: 1, fontWeight: "1000", fontSize: 22 }}> {props.title} </Typography>
+                        <Typography align="center" sx={{ m: 1 }}> {props.description} </Typography>
+                        <Typography align="center" sx={{ m: 1, display: "flex", flexDirection: "row", justifyContent: "center" }}> <QueryBuilderIcon style={{ color: "red", marginRight: 5 }} /> {props.duration + " " + (props.duration > 1 ? "weeks" : "week")}  </Typography>
+                        <Typography align="center" sx={{ m: 1, display: "flex", flexDirection: "row", justifyContent: "center" }}> <PaidIcon style={{ color: "red", marginRight: 5 }} /> {props.compensation + " $"}  </Typography>
+                        <Grid container style={{ marginTop: 3, display: "flex", flexDirection: "row", justifyContent: "center" }} spacing={3}>
+                            {
+                                props.skills.map((el, index) => (
+                                    <Chip key={index} style={{ marginLeft: 7, marginTop: 7 }} variant="outlined" label={el.name} />
+
+                                ))
+                            }
+                        </Grid>
+                        <Grid container style={{ marginTop: 3, display: "flex", flexDirection: "row", justifyContent: "center" }} spacing={3}>
+                            {
+                                props.categories.map((el, index) => (
+                                    <Chip key={index} style={{ marginLeft: 7, marginTop: 7 }} variant="outlined" label={el.name} />
+
+                                ))
+                            }
+                        </Grid>
+
+                    </Grid>
+
+
+
+                </Paper>
+            </React.Fragment>
+
+        </Container >
+    );
+}
