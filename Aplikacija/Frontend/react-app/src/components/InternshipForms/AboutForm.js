439c0a765a3ba38611c40595df7f1e08e1af808e
Djordje Antic <djordje.antic@elfak.rs>
2022-06-27 00:39:07 +0200

    [Frontend] Implementacija InternshipCreator forme

--- Aplikacija/Frontend/react-app/src/components/InternshipForms/AboutForm.js
+++ Aplikacija/Frontend/react-app/src/components/InternshipForms/AboutForm.js
@@ -5,12 +5,55 @@ import { FieldArray, useFormikContext } from 'formik';
 import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
 import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
 import SchoolRoundedIcon from '@mui/icons-material/SchoolRounded';
+import ChipTransferList from './ChipTransferList';
 
 export default function AboutForm() {
 
 
     const { values } = useFormikContext();
     
+    const [skillData, setSkillData] = useState(null);
+    const getSkills = async () => {
+      const response = await fetch(
+        "http://localhost:7240/CV/GetSkills",
+        {
+          credentials: "include",
+        }
+      );
+      if (response.ok) {
+        const fetchData = await response.json();
+        console.log(fetchData);
+        setSkillData(fetchData.skills
+          .map((cards, index) =>
+          ({
+            id: cards.id,
+            label: cards.name
+          })
+          ));
+      }
+    };
+
+    
+    const [categoryData, setCategoryData] = useState(null);
+    const getCategories = async () => {
+      const response = await fetch(
+        "http://localhost:7240/Internship/Categories",
+        {
+          credentials: "include",
+        }
+      );
+      if (response.ok) {
+        const fetchData = await response.json();
+        console.log(fetchData);
+        setCategoryData(fetchData.categories
+          .map((cards, index) =>
+          ({
+            id: cards.id,
+            label: cards.name
+          })
+          ));
+      }
+    };
 
     return (
         <React.Fragment>
@@ -40,6 +83,22 @@ export default function AboutForm() {
                         <TextInputField name={"compensation"} label={"Compensation"} fullWidth />
                     </Grid>
                     
+                    <Grid container spacing={3} sx={{ mb: 4 }}>
+
+                <Grid item xs={12} >
+
+                    <Paper
+                        sx={{ p: 3 }}
+                        variant="outlined"
+                    >
+                        <Divider sx={{ mb: 3 }} >Internship Skills</Divider>
+                        <ChipTransferList chipData={skillData} leftTitle={"Choose skills:"} rightTitle={"Added skills:"} fieldName={"skills"} />
+                        <Divider sx={{ mt: 5, mb: 3 }} >Internship Categories</Divider>
+                        <ChipTransferList chipData={categoryData} leftTitle={"Choose category of work:"} rightTitle={"Added categories:"} fieldName={"categories"} />
+                        
+                    </Paper>
+                </Grid >
+            </Grid >
                 </Grid>
             </Paper>
         </React.Fragment>
